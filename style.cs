54b04344d3a65bada88b2b958f59563a16e952b7
polli2203 <146850815+polli2203@users.noreply.github.com>
2024-09-26 10:13:41 -0300

    cccc

--- style.cs
+++ style.cs
@@ -1,4 +1,6 @@
-@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap');
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Edu+AU+VIC+WA+NT+Guides:wght@400..700&display=swap');
+</style>
 
 :root {
     --bg-color: #222831;
