3eaa485a2f75ba5c32f1837b69a427cf7b92f784
polli2203 <146850815+polli2203@users.noreply.github.com>
2024-10-10 10:35:29 -0300

    gm

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,6 +4,7 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     const praticamRegularmente = (dados.praticam_regularmente_esportes/ 1e9)
+    const assistemEsportes = (dados.total_pessoas_que_assistem_esportes/ 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio_semana_praticando_esporte - horas) * 100)
@@ -11,7 +12,7 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${praticam_regularmente_esportes} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${praticam_regularmente_esportes} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social, enqunto isso, <span>${assistemEsportes} bilhões assistem a algum esporte. .`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
