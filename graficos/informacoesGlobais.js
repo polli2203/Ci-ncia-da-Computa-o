674ba98b88b7fa741214c21a99f9695bcabc9a34
polli2203 <146850815+polli2203@users.noreply.github.com>
2024-10-10 13:33:03 -0300

    jii

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,12 +6,11 @@ async function vizualizarInformacoesGlobais() {
     const praticamRegularmente = (dados.praticam_regularmente_esportes/ 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio_semana_praticando_esportes)
-    const minutos = Math.round((dados.tempo_medio_semana_praticando_esportes - horas) * 100)
     const porcentagemEsportes = ((praticamRegularmente / pessoasNoMundo ) * 100).toFixed(2)
-
+    const pessoasAssitem = (dados.total_pessoas_que_assistem_esportes/ 1e9)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${praticamRegularmente} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemEsportes}%</span> de pessoas estão conectadas em alguma rede social.`
+    paragrafo.innerHTML = `No mundo dos esportes, à nível global onde temos <span>${pessoasNoMundo} bilhões</span> de pessoas, das quais <span>${praticamRegularmente} bilhões</span> de pessoas praticam esportes regularmente, onde despendem aproximadamente <span>${horas} horas</span> semanais para praticar esportes.<br>Isso significa que aproximadamente <span>${porcentagemEsportes}%</span> das pessoas praticam algum esporte. Já as pessoas que assistem esportes, gira em média de <span>${pessoasAssitem} bilhões</span> de pessoas`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
