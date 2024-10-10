0657ff55220dda96a31aab6964297b193526a02d
polli2203 <146850815+polli2203@users.noreply.github.com>
2024-10-10 13:16:23 -0300

    b

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,11 +1,7 @@
 import { getCSS, tickConfig } from "./common.js"
 
 async function quantidadeUsuariosPorRede() {
-<<<<<<< HEAD
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
-=======
     const url = 'https://raw.githubusercontent.com/polli2203/Ci-ncia-da-Computa-o/refs/heads/main/esportes/esportes-mais-praticados(1).json'
->>>>>>> 02b401450a390b3281a7f720ab8d603c586cb177
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
