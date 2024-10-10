3f1668c3c26df321bc87f1b1b29531c041347ec1
polli2203 <146850815+polli2203@users.noreply.github.com>
2024-10-10 10:32:52 -0300

    cfg

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,11 +3,11 @@ const url = 'https://raw.githubusercontent.com/polli2203/Ci-ncia-da-Computa-o/re
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const praticam_regularmente_esportes = (dados.praticam_regularmente_esportes/ 1e9)
+    const praticamRegularmente = (dados.praticam_regularmente_esportes/ 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio_semana_praticando_esporte - horas) * 100)
-    const porcentagemConectada = ((praticam_regularmente_esportes / pessoasNoMundo ) * 100).toFixed(2)
+    const porcentagemConectada = ((praticamRegularmente / pessoasNoMundo ) * 100).toFixed(2)
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
